840134a0f71f0305096e6ca632206c6093932f77
WinterGuardian <a.w1nter@hotmail.com>
2016-05-09 14:37:58 -0400

    Startet Raceline & KartGame

--- core/src/org/wintermartens/kartgame/KartGame.java
+++ core/src/org/wintermartens/kartgame/KartGame.java
@@ -3,24 +3,39 @@ package org.wintermartens.kartgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KartGame extends ApplicationAdapter
 {
+	private SpriteBatch batch;
+	private Map<String, Texture> textures;
+
 	private Kart kart;
 	private Race race;
 
 	@Override
 	public void create()
 	{
+		this.textures = new HashMap<String, Texture>();
 
+		textures.put("kart", new Texture("kart.png"));
+		//this.race = new Race();
+		//this.kart = new Kart(this, );
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render()
 	{
+		batch.begin();
 
+		batch.end();
+		kart.update(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
@@ -37,6 +52,14 @@ public class KartGame extends ApplicationAdapter
 	private void start()
 	{
 
+	}
+
+	public Texture getTexture(String key)
+	{
+		if(textures.containsKey(key))
+			return textures.get(key);
+
+		return new Texture(new Pixmap(128, 128, Pixmap.Format.Alpha));
 	}
 
 	public Kart getKart()
--- core/src/org/wintermartens/kartgame/RaceLine.java
+++ core/src/org/wintermartens/kartgame/RaceLine.java
@@ -1,8 +1,63 @@
 package org.wintermartens.kartgame;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
- * Created by Cedric Martens on 2016-05-09.
+ * Represents a checkpoint for the race
+ * Contains 2 locations being the extremities of the line to cross
+ * Also used to replace players on the race
+ *
+ * Created by Alexander Winter on 2016-05-09.
  */
 public class RaceLine
 {
+	private Race race;
+	private Vector2 loc1, loc2;
+
+	public RaceLine(Race race, Vector2 loc1, Vector2 loc2)
+	{
+		this.race = race;
+		this.loc1 = loc1;
+		this.loc2 = loc2;
+	}
+
+	public boolean cross(Kart kart, Vector2 vector)
+	{
+		return false;
+	}
+
+	public Vector2 getCenter()
+	{
+		return new Vector2(loc1.x + (loc2.x - loc1.x) / 2, loc1.y + (loc2.y - loc1.y) / 2);
+	}
+
+	public Race getRace()
+	{
+		return race;
+	}
+
+	public void setRace(Race race)
+	{
+		this.race = race;
+	}
+
+	public Vector2 getLoc1()
+	{
+		return loc1;
+	}
+
+	public void setLoc1(Vector2 loc1)
+	{
+		this.loc1 = loc1;
+	}
+
+	public Vector2 getLoc2()
+	{
+		return loc2;
+	}
+
+	public void setLoc2(Vector2 loc2)
+	{
+		this.loc2 = loc2;
+	}
 }
