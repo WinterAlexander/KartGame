4c8b189a32665303a6d7c0384c9940117b8b6083
Alexander Winter <a.w1nter@hotmail.com>
2016-05-08 20:10:45 -0400

    Created LibGDX project and added UML plan

---
+++ desktop/src/org/wintermartens/kartgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.wintermartens.kartgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.wintermartens.kartgame.KartGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KartGame(), config);
+	}
+}
---
+++ html/src/org/wintermartens/kartgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.wintermartens.kartgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.wintermartens.kartgame.KartGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new KartGame();
+        }
+}
