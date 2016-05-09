df13013ba1c7530c9a6b2efe79760a672946c746
MartensCedric <cedricmartens98@gmail.com>
2016-05-09 11:13:26 -0400

    Edited desktop launcher and KartGame class

--- core/src/org/wintermartens/kartgame/KartGame.java
+++ core/src/org/wintermartens/kartgame/KartGame.java
@@ -6,22 +6,24 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class KartGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class KartGame extends ApplicationAdapter
+{
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create()
+	{
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render()
+	{
+
+	}
+
+	@Override
+	public void dispose()
+	{
+		super.dispose();
 	}
 }
--- desktop/src/org/wintermartens/kartgame/desktop/DesktopLauncher.java
+++ desktop/src/org/wintermartens/kartgame/desktop/DesktopLauncher.java
@@ -4,9 +4,24 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.wintermartens.kartgame.KartGame;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+import java.awt.*;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		config.width = 1280;
+		config.height = 720;
+
+		while(config.width > screenSize.width - 64 || config.height > screenSize.height - 64)
+		{
+			config.width -= 16;
+			config.height -= 9;
+		}
 		new LwjglApplication(new KartGame(), config);
 	}
 }
